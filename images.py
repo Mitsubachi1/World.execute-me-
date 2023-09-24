41c2f6c5f8569cdba74fef33e649de01a68f08b9
Mitsubachi1 <60766345+Mitsubachi1@users.noreply.github.com>
2023-09-24 13:19:20 -0700

    .

---
+++ images.py
@@ -0,0 +1,6 @@
+from PIL import Image
+import sys
+int_dir = sys.MEIPASS
+path = os.path.join
+img = Image.open()
+img.show()
---
+++ worldExecuteMe.py
@@ -0,0 +1,10 @@
+import time
+
+me = "World.Executr(me);"
+
+class World:
+
+    def Execution(me):
+        print(me)
+
+World.Execution(me)
